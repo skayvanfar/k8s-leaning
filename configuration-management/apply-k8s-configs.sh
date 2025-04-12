5ddcd6b0e2c30756e624e54627daf447ba73dfb8
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2025-04-12 16:09:58 +0200

    add Makefile to run my-namespace resources

--- configuration-management/apply-k8s-configs.sh
+++ configuration-management/apply-k8s-configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the directory containing YAML files
-CONFIG_DIR="./k8s-configs/my-namespace"
+CONFIG_DIR="../k8s-configs/my-namespace"
 
 # Apply all YAML files in the directory
 for file in $CONFIG_DIR/*.yaml; do
