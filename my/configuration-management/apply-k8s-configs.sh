596b96f5392a907ecf6f49e1cd80dbcec34bfdc2
Saeid Keyvanfar <kayvanfar.sj@gmail.com>
2025-04-12 20:37:51 +0200

    add my-deployment.yaml to my-namespace

--- my/configuration-management/apply-k8s-configs.sh
+++ my/configuration-management/apply-k8s-configs.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Path to the directory containing YAML files
-CONFIG_DIR="../k8s-configs/my-namespace"
+CONFIG_DIR="./k8s-configs/my-namespace"
+
+# Apply the namespace first
+kubectl apply -f ./k8s-configs/my-namespace/my-namespace.yaml
+
+# Wait for the namespace to be created (you can increase the timeout if needed)
+kubectl wait --for=condition=Established namespace/my-namespace --timeout=1s
 
 # Apply all YAML files in the directory
 for file in $CONFIG_DIR/*.yaml; do
